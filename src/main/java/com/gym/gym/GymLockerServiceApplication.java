a4bf7770f86aee02bb6d49e0b8d6060dfd6d72e9
KryMaj <krystianmajda96@gmail.com>
2022-11-22 22:33:11 +0100

    gym locker app

---
+++ src/main/java/com/gym/gym/GymLockerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.gym.gym;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymLockerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GymLockerServiceApplication.class, args);
+    }
+
+}
